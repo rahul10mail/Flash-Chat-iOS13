399ce20fb7e68c4cc58fae27fec5670fcd6ca32d
Rahul Kumar <rahul10mail@gmail.com>
2020-04-30 08:47:34 +0530

    Created UITableView and a message model

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -13,8 +13,12 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [Message(sender: "rahul@xyz.com", body: "Hi"), Message(sender: "1@xyz.com", body: "bye"), Message(sender: "2@xyz.com", body: "Thik hai")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+//        tableView.delegate = self
         title = Constants.appName
         navigationItem.hidesBackButton = true
     }
@@ -34,3 +38,24 @@ class ChatViewController: UIViewController {
     }
     
 }
+
+//MARK: - UITableViewDataSource
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        cell.detailTextLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+}
+
+//MARK: - UITableViewDelegate
+//extension ChatViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+//}
